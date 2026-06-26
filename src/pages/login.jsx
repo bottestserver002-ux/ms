9e95228b399f85f1fccea1f9a3aab6ed3ee6eb77
ManhTruong-SsE <nguyentruong2003ct@gmail.com>
2026-06-26 15:05:15 +0700

    aaaa

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -163,7 +163,7 @@ export default function Login() {
           otp,
         });
 
-        alert(data.message);
+        alert(data.message || data.detail || "Có lỗi xảy ra");
 
         if (
           data.message?.includes("thành công")
